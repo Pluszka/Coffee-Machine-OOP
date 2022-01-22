6fc5dbe2da967fd40560ebf6559cb5c57d6f7694
Pliszka <karoliniakkjk@gmail.com>
2022-01-22 21:12:57 +0100

    def whatToDo  checking user input

--- main.py
+++ main.py
@@ -2,3 +2,15 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+
+def whatToDo():
+    choiceOption=['1', '2', '3', 'report', 'off']
+    while not choose in choiceOption:
+        choose=input('What would you like? (espresso(1)/latte(2)/cappuccino(3)):')
+    return choose
+
+def analyzing(choosenOption):
+
+
+
+analyzing(whatToDo())
