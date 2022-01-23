a3d648ee71953449c2f210af360ad7b45fdc54fb
Pliszka <karoliniakkjk@gmail.com>
2022-01-23 20:10:25 +0100

    finished version

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ def whatToDo():
         choose=input(f'What would you like?:{menu.get_items()}\n1. $2.5\n2. $1.5\n3. $3\n')
     return choose
 
-
 def analyzing(choosenOption):
     coffeeType={'1':'espresso', '2':'latte', '3':'cappuccino'}
     if choosenOption=='report':
@@ -25,15 +24,13 @@ def analyzing(choosenOption):
     else:
         makeDrink(menu.find_drink(coffeeType[choosenOption]))
 
-
 def escape():
     global on
     on=False
 
 def makeDrink(typeOfCoffee):
-    if coffee.is_resource_sufficient(typeOfCoffee):
-        if money.make_payment(typeOfCoffee.cost):
-            coffee.make_coffee(typeOfCoffee)
+    if coffee.is_resource_sufficient(typeOfCoffee) and money.make_payment(typeOfCoffee.cost):
+        coffee.make_coffee(typeOfCoffee)
 
 def machine():
     while on:
